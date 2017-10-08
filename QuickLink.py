224b2d943f9efc2008dd7ed8b63a576e1e0c93a1
AlecMMiller <alecmichaelmiller@gmail.com>
2017-10-07 18:14:20 -0700

    Added FrameData object

--- QuickLink.py
+++ QuickLink.py
@@ -4,9 +4,99 @@ dll = CDLL('DLL\QuickLink2.dll')
 
 
 def get_version():
-    buffer_size = 32
     func = dll.QLAPI_GetVersion
+    buffer_size = 32
     func.argtypes = [c_int, c_char_p]
     output = create_string_buffer(buffer_size)
     func(c_int(buffer_size), output)
     return output.value.decode('UTF-8')
+
+
+def device_enumerate():
+    func = dll.QLDevice_Enumerate
+    buffer_size = 5
+    num_devices = c_int(buffer_size)
+    num_devices_pointer = pointer(num_devices)
+    device_buffer = (c_int * buffer_size)()
+    device_buffer_pointer = pointer(device_buffer)
+    func(num_devices_pointer, device_buffer_pointer)
+    number = num_devices.value
+    device_list = []
+    for i in range(0, number):
+        device_list.append(device_buffer[i])
+    return device_list
+
+
+class QLXYPairFloat(Structure):
+    _fields_ = [
+        ("x", c_float),
+        ("y", c_float)
+    ]
+
+
+class QLEyeData(Structure):
+    _fields_ = [
+        ("Found", c_bool),
+        ("Calibrated", c_bool),
+        ("PupilDiameter", c_float),
+        ("Pupil", QLXYPairFloat),
+        ("Glint0", QLXYPairFloat),
+        ("Glint1", QLXYPairFloat),
+        ("GazePoint", QLXYPairFloat)
+        # Reserved 16 possibly needed here
+    ]
+
+
+class QLWeightedGazePoint(Structure):
+    _fields_ = [
+        ("Valid", c_bool),
+        ("x", c_float),
+        ("y", c_float),
+        ("LeftWeight", c_float),
+        ("RightWeight", c_float)
+        # Reserved 16 possibly needed here
+    ]
+
+
+class QLRectInt(Structure):
+    _fields_ = [
+        ("x", c_int),
+        ("y", c_int),
+        ("width", c_int),
+        ("height", c_int)
+    ]
+
+
+class QLImageData(Structure):
+    _fields_ = [
+        ("PixelData", c_char_p),
+        ("Width", c_int),
+        ("Height", c_int),
+        ("Timestamp", c_double),
+        ("Gain", c_int),
+        ("FrameNumber", c_long),
+        ("ROI", QLRectInt),
+        ("ScaleFactor", c_float)
+    ]
+
+
+class FrameData(Structure):
+    _fields_ = [
+        ("ImageData", QLImageData),
+        ("LeftEye", QLEyeData),
+        ("RightEye", QLEyeData),
+        ("WeightedGazePoint", QLWeightedGazePoint),
+        ("Focus", c_float),
+        ("Distance", c_float),
+        ("Bandwidth", c_int),
+        ("DeviceID", c_int),
+        # Will likely need a 15 long void* here called "Reserved"
+    ]
+
+
+def get_frame(identifier):
+    func = dll.QLDevice_GetFrame
+    device_or_group = c_int(identifier)
+    frame = FrameData()
+    print(func(device_or_group, c_int(1000), frame))
+    print(frame.DeviceID)
