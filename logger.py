2b9293eb25967a487a7e2adf24c6d7e397c5fa2b
AlecMMiller <alecmichaelmiller@gmail.com>
2017-10-09 14:43:31 -0700

    Logger now has timestamp

--- logger.py
+++ logger.py
@@ -3,9 +3,11 @@
 # Level 2 - Basic Logging
 # Level 3 - Everything
 
+from datetime import datetime
+
 severity_threshold = 3
 
 
 def log(message, severity):
     if severity >= severity_threshold:
-        print(message)
+        print(str(datetime.now()) + " - " + message)
