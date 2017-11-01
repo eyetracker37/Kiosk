205f4e07898e1b8cf8a464ab8d21dbb758471cc3
AlecMMiller <alecmichaelmiller@gmail.com>
2017-11-01 10:45:07 -0700

    Logging data now gets machine info

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from Input import input_handler
 from Pages import map
 from Utils import config
+from Utils.logger import log
+import pygame
+import platform
+
+log("Platform " + platform.platform(), 1)
+log("Processor " + platform.machine(), 1)
+log("Python version " + platform.python_version(), 1)
+log("PyGame version " + pygame.version.ver, 1)
 
 config.initialize()
 input_handler.initialize()
