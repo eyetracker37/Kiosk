c325d91c566742a1af583f4a74faba76eab72783
AlecMMiller <alecmichaelmiller@gmail.com>
2017-11-09 11:17:00 -0700

    System info now saved to log

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from Pages import map
 from Utils import config
 from Utils.logger import log_sys_info
 
-log_sys_info()  # Logs information about system for debug purposes
-
 config.initialize()  # Loads config settings
+log_sys_info()  # Logs information about system for debug purposes
 input_handler.initialize()  # Starts up input handler
 
 map.run()  # Runs entry point for GUI
