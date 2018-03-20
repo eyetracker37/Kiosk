617435fb2b58a4f94c3f71a25800ca88367b833a
eearnol2 <eearnol2@asu.edu>
2018-03-19 17:34:54 -0700

    hola

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from Input import input_handler
+<<<<<<< .merge_file_a12432
 from Pages import map, info
+=======
+from Pages import Calibration_Screen
+>>>>>>> .merge_file_a19796
 from Utils import config
 from Utils.logger import log_sys_info
 from Elements import window_elements
@@ -8,9 +12,15 @@ config.initialize()  # Loads config settings
 log_sys_info()  # Logs information about system for debug purposes
 input_handler.initialize()  # Starts up input handler
 
+<<<<<<< .merge_file_a12432
 master = window_elements.MasterWindow()
 #info.run(master, "AravaipaAuditorium")  # Use this to enter directly to info screen
 map.run(master)  # Runs entry point for GUI
 
 window_elements.run_master(master)
 input_handler.close()  # If map.run() stops, close input handler
+=======
+Calibration_Screen.run()  # Runs entry point for GUI
+
+input_handler.close()  # If Calibration_Screen.run() stops, close input handler
+>>>>>>> .merge_file_a19796
